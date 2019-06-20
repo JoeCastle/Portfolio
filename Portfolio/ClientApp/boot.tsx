acda6d4ce6b0e65ee212462556e1f46ff006812e
JoeCastle <joecastle1997@hotmail.com>
2019-06-20 15:32:03 +0100

    Add project files.

---
+++ Portfolio/ClientApp/boot.tsx
@@ -0,0 +1,44 @@
+//import './css/site.css';
+//require('core-js/modules/es6.symbol');
+//import 'babel-polyfill';
+//import 'core-js/es/map';
+//import 'core-js/es/set';
+
+/*import 'react-app-polyfill/ie11';
+import 'core-js/features/array/find';
+import 'core-js/features/array/includes';
+import 'core-js/features/number/is-nan';*/
+
+//import 'core-js/es6/symbol';
+
+import './scss/site.scss';
+import 'bootstrap';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import { BrowserRouter } from 'react-router-dom';
+import * as RoutesModule from './routes';
+
+let routes = RoutesModule.routes;
+
+function renderApp() {
+    // This code starts up the React app when it runs in a browser. It sets up the routing
+    // configuration and injects the app into a DOM element.
+    const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href')!;
+    ReactDOM.render(
+        <AppContainer>
+            <BrowserRouter children={ routes } basename={ baseUrl } />
+        </AppContainer>,
+        document.getElementById('react-app')
+    );
+}
+
+renderApp();
+
+// Allow Hot Module Replacement
+if (module.hot) {
+    module.hot.accept('./routes', () => {
+        routes = require<typeof RoutesModule>('./routes').routes;
+        renderApp();
+    });
+}
---
+++ Portfolio/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
+const bundleOutputDir = './wwwroot/dist';
+
+module.exports = (env) => {
+    const isDevBuild = !(env && env.prod);
+    return [{
+        stats: { modules: false },
+        entry: { 'main': './ClientApp/boot.tsx' },
+        resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+        output: {
+            path: path.join(__dirname, bundleOutputDir),
+            filename: '[name].js',
+            publicPath: 'dist/'
+        },
+        module: {
+            rules: [
+                { test: /\.tsx?$/, include: /ClientApp/, use: 'awesome-typescript-loader?silent=true' },
+                //{ test: /\.css$/, use: isDevBuild ? ['style-loader', 'css-loader'] : ExtractTextPlugin.extract({ use: 'css-loader?minimize' }) },
+                //https://medium.com/@realfrancisyan/scss-autoprefixer-with-webpack-9f8456ae751b
+                { test: /\.(s*)css$/, use: [{ loader: 'style-loader' }, { loader: 'css-loader'}, { loader: 'postcss-loader', options: { config: { path: 'postcss.config.js' } } }, { loader: 'sass-loader' }] },
+                //{ test: /\.(s*)css$/, use: ['style-loader', 'css-loader', 'sass-loader'] }, //https://medium.com/a-beginners-guide-for-webpack-2/using-sass-9f52e447c5ae
+                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+            ]
+        },
+        plugins: [
+            new CheckerPlugin(),
+            new webpack.DllReferencePlugin({
+                context: __dirname,
+                manifest: require('./wwwroot/dist/vendor-manifest.json')
+            })
+        ].concat(isDevBuild ? [
+            // Plugins that apply in development builds only
+            new webpack.SourceMapDevToolPlugin({
+                filename: '[file].map', // Remove this line if you prefer inline source maps
+                moduleFilenameTemplate: path.relative(bundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
+            })
+        ] : [
+            // Plugins that apply in production builds only
+            new webpack.optimize.UglifyJsPlugin(),
+            new ExtractTextPlugin('site.css')
+        ])
+    }];
+};
