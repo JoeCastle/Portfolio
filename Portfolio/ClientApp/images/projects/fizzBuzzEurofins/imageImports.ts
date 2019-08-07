efabde633784a4e40b6ec0c04ede905bc1d0e045
JoeCastle <joecastle1997@hotmail.com>
2019-08-07 02:23:35 +0100

    Linked previously unlinked fizzbuzz task images up to project summary component.

---
+++ Portfolio/ClientApp/images/projects/fizzBuzzEurofins/imageImports.ts
@@ -0,0 +1,11 @@
+﻿import fizzBuzzEurofinsImg1 from './fizzBuzzEurofins_img_1.JPG';
+import fizzBuzzEurofinsImg2 from './fizzBuzzEurofins_img_2.JPG';
+import fizzBuzzEurofinsImg3 from './fizzBuzzEurofins_img_3.JPG';
+
+const images = {
+    fizzBuzzEurofinsImg1,
+    fizzBuzzEurofinsImg2,
+    fizzBuzzEurofinsImg3,
+}
+
+export default images;
--- Portfolio/ClientApp/images/projects/imageImports.ts
+++ Portfolio/ClientApp/images/projects/imageImports.ts
@@ -1,10 +1,12 @@
 ﻿import arduinoTemperatureSensor from './arduinoTemperatureSensor/imageImports';
 import arlaBobImages from './arlaBob/imageImports';
+import fizzBuzzEurofinsImages from './fizzBuzzEurofins/imageImports';
 import shoothillWebsiteImages from './shoothillWebsite/imageImports';
 
 const images = {
     arduinoTemperatureSensor,
     arlaBobImages,
+    fizzBuzzEurofinsImages,
     shoothillWebsiteImages
 }
 
